f21acef8cad07158462f156c05df91a9e2750905
Truuuuuuuu <jethruel@gmail.com>
2026-08-08 00:29:35 +0800

    implement get all subject route, set up cors

---
+++ src/routes/subject.ts
@@ -0,0 +1,71 @@
+import express from "express";
+import { or, ilike, and, sql, eq, getTableColumns, desc } from "drizzle-orm";
+import { subjects, departments } from "../db/schema/index.js";
+import { db } from "../db/index.js";
+
+const router = express.Router();
+
+//get alls subjest w/ optional search, filtering and pagination
+router.get("/", async (req, res) => {
+  try {
+    const { search, department, page = 1, limit = 10 } = req.query;
+
+    const currentPage = Math.max(1, +page);
+    const limitPerPage = Math.max(1, +limit);
+
+    const offset = (currentPage - 1) * limitPerPage;
+
+    const filterConditions = [];
+
+    if (search) {
+      filterConditions.push(
+        or(
+          ilike(subjects.name, `%${search}%`),
+          ilike(subjects.code, `%${search}%`),
+        ),
+      );
+    }
+
+    if (department) {
+      filterConditions.push(or(ilike(departments.name, `%${department}%`)));
+    }
+
+    const whereClause =
+      filterConditions.length > 0 ? and(...filterConditions) : undefined;
+
+    const countResult = await db
+      .select({ count: sql<number>`count(*)` })
+      .from(subjects)
+      .leftJoin(departments, eq(subjects.departmentId, departments.id))
+      .where(whereClause);
+
+    const totalCount = countResult[0]?.count ?? 0;
+
+    const subjectsList = await db
+      .select({
+        ...getTableColumns(subjects),
+        department: { ...getTableColumns(departments) },
+      })
+      .from(subjects)
+      .leftJoin(departments, eq(subjects.departmentId, departments.id))
+      .where(whereClause)
+      .orderBy(desc(subjects.createdAt))
+      .limit(limitPerPage)
+      .offset(offset);
+
+    res.status(200).json({
+      data: subjectsList,
+      pagination: {
+        page: currentPage,
+        limit: limitPerPage,
+        total: totalCount,
+        totalPages: Math.ceil(totalCount / limitPerPage),
+      },
+    });
+  } catch (e) {
+    console.error(`Error while getting subjects: ${e}`);
+    res.status(500).json({ error: "Failed to get subjects" });
+  }
+});
+
+export default router;
